9343074928c21671256b09cd98a2a59d6bd29110
EC2 Default User <114875327+kana6234@users.noreply.github.com>
2022-10-30 12:47:26 +0000

    [add]homesとitemsのコントローラの作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,15 @@ Rails.application.routes.draw do
   sessions: 'public/sessions'
   }
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
-  sessions: "admin/sessions"
+  sessions: 'admin/sessions'
   }
+  namespace :public do
+    root 'homes#top'
+    get '/about' => 'homes#about'
+  end
+
+  namespace :public do
+    resources :items, only:[:index, :show]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
