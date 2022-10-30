8cb91215fa11fab58a713b3903cc676da37a1636
EC2 Default User <114875327+kana6234@users.noreply.github.com>
2022-10-30 14:53:57 +0000

    [add]adminのcustomersとordersとorder_datailsのコントローラの作成

--- config/routes.rb
+++ config/routes.rb
@@ -24,8 +24,11 @@ Rails.application.routes.draw do
   end
   namespace :admin do
     get 'top' => 'homes#top'
-    resources :adresses, only:[:new, :index, :create, :show, :edit, :update]
-    resources :adresses, only:[:index, :create, :edit, :update]
+    resources :items, only:[:new, :index, :create, :show, :edit, :update]
+    resources :genres, only:[:index, :create, :edit, :update]
+    resources :custmers, only:[:index, :show, :edit, :update]
+    resources :orders, only:[:show, :update]
+    resources :order_datails, only:[:show, :update]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
