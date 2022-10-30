74df7812575ffa057d29ad619a93440e6228607f
EC2 Default User <114875327+kana6234@users.noreply.github.com>
2022-10-30 14:29:32 +0000

    [fix]publicとadminのroutesが正しくない不具合の修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :customers, skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
+  registrations: "custmers/registrations",
+  sessions: 'custmers/sessions'
   }
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
   sessions: 'admin/sessions'
   }
-  namespace :public do
+  scope module: :public do
     root 'homes#top'
     get 'about' => 'homes#about'
     resources :items, only:[:index, :show]
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
     get 'orders/complete' => 'orders#complete'
     resources :adresses, only:[:index, :create, :edit, :update, :destroy]
   end
-  namespace :public do
+  namespace :admin do
     get 'top' => 'homes#top'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
