116db2312e2e1ad2e26998d4b318a7ccbf6ad2e6
EC2 Default User <114875327+kana6234@users.noreply.github.com>
2022-10-31 13:04:48 +0000

    [add]ヘッダーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :customers, skip: [:passwords], controllers: {
-  registrations: "custmers/registrations",
-  sessions: 'custmers/sessions'
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
   }
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
   sessions: 'admin/sessions'
