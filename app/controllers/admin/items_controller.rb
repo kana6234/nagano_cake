9a510b357fc2ebc35031f59db18d5d91f50720fa
EC2 Default User <114875327+kana6234@users.noreply.github.com>
2022-11-02 14:25:24 +0000

    [add]商品新規登録と商品一覧ページの作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,11 +1,21 @@
 class Admin::ItemsController < ApplicationController
   def new
+    @item = Item.new
+    @genres = Genre.all
   end
 
   def index
+    @items = Item.all
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_items_path
+    else
+      @items = Item.all
+      render :index
+    end
   end
 
   def show
@@ -15,5 +25,11 @@ class Admin::ItemsController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :genre_id, :price, :is_active)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,11 @@ class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :order_items, dependent: :destroy
   belongs_to :genre
+  has_one_attached :image
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :genre_id, presence: true
+  validates :price, presence: true
+  validates :is_active, presence: true
 end
