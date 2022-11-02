ed687e6711f8bd079e83348f20a534a3bf977e81
EC2 Default User <114875327+kana6234@users.noreply.github.com>
2022-11-02 11:59:20 +0000

    [add]モデルの関連付けの追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum status: { awaiting_payment: 0, confirmation_payment: 1, production_now: 2, preparing_now: 3,shipped: 4 }
+  has_many :order_items, dependent: :destroy
+  belongs_to :customer
 end
