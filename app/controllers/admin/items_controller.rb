f3e15ae92bcf5d16be5ba35470ee051b949a0138
EC2 Default User <114875327+kana6234@users.noreply.github.com>
2022-11-03 12:06:46 +0000

    [add]商品編集ページの作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -23,9 +23,18 @@ class Admin::ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+    @genres = Genre.all
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_item_path(@item.id)
+    else
+      @genres = Genre.all
+      render :edit
+    end
   end
 
   private
