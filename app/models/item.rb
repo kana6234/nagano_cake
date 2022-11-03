3931b8f22f0df92d3bb1b4e1a2aaabff54bb65e8
EC2 Default User <114875327+kana6234@users.noreply.github.com>
2022-11-03 11:52:53 +0000

    [update]消費税の計算をmodelに移動

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,7 +14,7 @@ class Item < ApplicationRecord
     image.variant(resize_to_limit: [width, height]).processed
   end
 
-  def tax_price
-        (self.price * 1.1).round
+  def with_tax_price
+    (price * 1.1).floor
   end
 end
