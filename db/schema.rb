28f467ebec75b16f19fd857a0c6df21520031f16
EC2 Default User <114875327+kana6234@users.noreply.github.com>
2022-11-04 14:58:40 +0000

    [fix]OrderItemモデルのamountカラムの誤字の修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_27_160721) do
+ActiveRecord::Schema.define(version: 2022_11_04_145411) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -114,7 +114,7 @@ ActiveRecord::Schema.define(version: 2022_10_27_160721) do
     t.integer "item_id", null: false
     t.integer "order_id", null: false
     t.integer "price", null: false
-    t.integer "amont", null: false
+    t.integer "amount", null: false
     t.integer "making_status", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
