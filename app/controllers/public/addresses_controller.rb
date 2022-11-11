f7991c199d2d691551ab8628e52bdf1e1b1ef5c7
EC2 Default User <114875327+kana6234@users.noreply.github.com>
2022-11-11 15:44:24 +0000

    [add]顧客の配送先新規登録/一覧と編集ページの作成

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,16 +1,42 @@
 class Public::AddressesController < ApplicationController
   def index
+    @address = Address.new
+    @addresses = current_customer.addresses
   end
 
   def create
+    @address = current_customer.addresses.new(address_params)
+    if @address.save
+      redirect_to addresses_path
+    else
+      @address = Address.new
+      @addresses = current_customer.addresses
+      render :index
+    end
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      redirect_to addresses_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    address = Address.find(params[:id])
+    address.destroy
+    redirect_to addresses_path
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:postal_code, :address, :name)
   end
 end
