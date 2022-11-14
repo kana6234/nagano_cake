bc9e2022198ac34b42fc16d81f2135e6ae607dd8
EC2 Default User <114875327+kana6234@users.noreply.github.com>
2022-11-14 14:02:18 +0000

    [add]カートに商品を追加する機能の追加

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -3,6 +3,13 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
+    if !current_customer
+    redirect_to new_customer_session_path
+    else
+      @cart_item = current_customer.cart_items.new(cart_item_params)
+      @cart_item.save
+      redirect_to cart_items_path
+    end
   end
 
   def update
@@ -12,5 +19,11 @@ class Public::CartItemsController < ApplicationController
   end
 
   def destroy_all
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
   end
 end
