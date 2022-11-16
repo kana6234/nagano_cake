c54b2834734ae6b26c9d702c165d1c3a004f6bdc
EC2 Default User <114875327+kana6234@users.noreply.github.com>
2022-11-16 12:45:27 +0000

    [add]商品をカートに追加する時カート内に同一商品が存在する場合の処理の追加

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,15 +1,18 @@
 class Public::CartItemsController < ApplicationController
+  before_action :authenticate_customer!, only: [:create]
   def index
     @cart_items = current_customer.cart_items
     @total = @cart_items.inject(0) { |sum, item| sum + item.subtotal }
   end
 
   def create
-    if !current_customer
-    redirect_to new_customer_session_path
-    else
-      @cart_item = current_customer.cart_items.new(cart_item_params)
-      @cart_item.save
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+      cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.amount += params[:cart_item][:amount].to_i
+      cart_item.save
+      redirect_to cart_items_path
+    else @cart_item.save
       redirect_to cart_items_path
     end
   end
