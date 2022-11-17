b4a735afc6fd0eadbef0b10cabd6a406c9c54e6f
EC2 Default User <114875327+kana6234@users.noreply.github.com>
2022-11-17 15:42:24 +0000

    [add]注文情報確認ページの作成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -14,18 +14,19 @@ class Public::OrdersController < ApplicationController
   end
 
   def comfirm
+    @cart_items = current_customer.cart_items
+    @total = @cart_items.inject(0) { |sum, item| sum + item.subtotal }
     @order = Order.new(order_params)
     if params[:order][:select_address].to_i == 0
       @order.postal_code = current_customer.postal_code
       @order.address = current_customer.address
-      @order.name = current_customer.first_name + current_customer.last_name
+      @order.name = current_customer.last_name + current_customer.first_name
     elsif params[:order][:select_address].to_i == 1
       @address = Address.find(params[:order][:address_id])
       @order.postal_code = @address.postal_code
       @order.address = @address.address
       @order.name = @address.name
     end
-    redirect_to orders_comfirm_path
   end
 
   def complete
@@ -34,6 +35,6 @@ class Public::OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name, :postage)
   end
 end
