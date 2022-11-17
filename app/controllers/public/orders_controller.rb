24cb24306b3aa83723e1346aff48e567e6779239
EC2 Default User <114875327+kana6234@users.noreply.github.com>
2022-11-17 13:40:46 +0000

    [add]注文情報入力ページで入力された情報の取得機能の追加

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -14,9 +14,26 @@ class Public::OrdersController < ApplicationController
   end
 
   def comfirm
+    @order = Order.new(order_params)
+    if params[:order][:select_address].to_i == 0
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.first_name + current_customer.last_name
+    elsif params[:order][:select_address].to_i == 1
+      @address = Address.find(params[:order][:address_id])
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
+    end
     redirect_to orders_comfirm_path
   end
 
   def complete
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
   end
 end
