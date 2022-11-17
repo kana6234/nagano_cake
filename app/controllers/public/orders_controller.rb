760556fe7cc7ce936fca4a0a1356fc27ac1ca9a3
EC2 Default User <114875327+kana6234@users.noreply.github.com>
2022-11-17 12:35:09 +0000

    [add]注文情報入力ページの作成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,5 +1,7 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order = Order.new
+    @customer = Customer.find(current_customer.id)
   end
 
   def index
@@ -12,6 +14,7 @@ class Public::OrdersController < ApplicationController
   end
 
   def comfirm
+    redirect_to orders_comfirm_path
   end
 
   def complete
