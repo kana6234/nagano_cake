a4ed83c5dfb98db7f57247ab1a043b4168d8d579
EC2 Default User <114875327+kana6234@users.noreply.github.com>
2022-11-20 13:38:54 +0000

    [change]管理者ログイン時の遷移先の変更とtopページの新着商品から商品詳細ページへ飛べるように変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    if admin_signed_in?
+      admin_top_path
+    else
+      root_path
+    end
+  end
   def total
     @total = current_customer.cart_items.inject(0) { |sum, item| sum + item.subtotal }
   end
